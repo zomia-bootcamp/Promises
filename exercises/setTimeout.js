a96679390216753caa6ec4601a538afcc0e5b1fa
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-10 11:23:18 +0300

    Create setTimeout.js

---
+++ exercises/setTimeout.js
@@ -0,0 +1,16 @@
+/*
+
+1. Create a function called `useSTO()`. 
+- Your `useSTO()` function should have no parameters. 
+- Inside the function body, it should print a string to the console. This can be any string you want.
+
+Check your work to move on to the next step.
+
+2. Invoke the setTimeout() function. Remember, setTimeout() has two parameters. 
+Invoke setTimeout() with your `useSTO()` function as the first argument and a number between 0 and 3000 as the second argument.
+
+3. STOP! Before you run your code, read it carefully and try to think, what will the otput look like?
+
+<your answer>
+
+4. After comleting your answer, run your code, did you get it right?
