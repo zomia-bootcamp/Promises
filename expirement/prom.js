78d9e2f53c33803a6eddbd38e9c587aa0dfdde0e
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-10 11:34:41 +0300

    Create prom.js

---
+++ expirement/prom.js
@@ -0,0 +1,16 @@
+```js
+const prom = new Promise((resolve, reject) => {
+  resolve('Yay!');
+});
+ 
+const handleSuccess = (resolvedValue) => {
+  console.log(resolvedValue);
+};
+ 
+prom.then(handleSuccess); // Prints: 'Yay!'
+```
+
+
+/*
+Run the code above and try to break down what's happening to get a better understanding of success handlers.
+Use chatGPT for help.
