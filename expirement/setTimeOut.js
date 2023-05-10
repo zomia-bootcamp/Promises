7340a782cef1438ff623124b47060b12092f70a3
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-05-10 11:04:29 +0300

    Create setTimeOut.js

---
+++ expirement/setTimeOut.js
@@ -0,0 +1,20 @@
+
+
+
+const delayedHello = () => {
+  console.log('Hi! This is an asynchronous greeting!');
+};
+ 
+setTimeout(delayedHello, 2000);
+
+/*
+In your own words, describe the way this code ran:
+
+<your answer>
+
+What do you think caused this behaviour?
+
+<your answer>
+
+Use chatGpt or your favorite search engine to undrestand the setTimeOut method.
+Focus on understanding the syntax and the process
