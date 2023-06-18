8930b2882013ee2daa80a3e546814a3d2dc95a96
zomia-bootcamp <132401198+zomia-bootcamp@users.noreply.github.com>
2023-06-18 07:27:39 +0300

    fixing close comment

--- expirement/prom.js
+++ expirement/prom.js
@@ -14,3 +14,4 @@ prom.then(handleSuccess); // Prints: 'Yay!'
 /*
 Run the code above and try to break down what's happening to get a better understanding of success handlers.
 Use chatGPT for help.
+*/
